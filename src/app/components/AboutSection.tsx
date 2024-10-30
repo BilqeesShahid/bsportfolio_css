3cc812c2e3505c2b49a5b60f23bea0e15864d999
Bilqees Shahid <161162316+BilqeesShahid@users.noreply.github.com>
2024-10-31 02:05:42 +0500

    Update AboutSection.tsx

--- src/app/components/AboutSection.tsx
+++ src/app/components/AboutSection.tsx
@@ -6,7 +6,7 @@ import React from 'react'
 
 interface PropsType{
   active: boolean; // Assuming it's a boolean
-  selectTab: void; // Adjust the argument type as necessary
+  selectTab: (isActive: boolean) => void; // Adjust the argument type as necessary
   children: React.ReactNode; // For any valid React node
 
 }
