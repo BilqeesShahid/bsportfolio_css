31c4f2c80c33258a57b0288c357b910d6812f272
Bilqees Shahid <161162316+BilqeesShahid@users.noreply.github.com>
2024-11-01 00:43:04 +0500

    Update EmailSection.tsx

--- src/app/components/EmailSection.tsx
+++ src/app/components/EmailSection.tsx
@@ -12,7 +12,7 @@ import { BsTelephone } from 'react-icons/bs';
 const EmailSection = () => {
   const [result, setResult] = useState("Send Message");
 
-  const sendMessage = async (event: { preventDefault: () => void; target: HTMLFormElement}) => {
+  const sendMessage = async (event) => {
     event.preventDefault();
     setResult("Sending....");
     const formData = new FormData(event.target);
